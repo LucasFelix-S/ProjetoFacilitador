4c208b217c5b75e02d989fa7df0491bf3b784959
LucasFelix-S <started.lucasfelixsantos@gmail.com>
2025-12-12 23:44:31 -0300

    core: organizando a lógica para a aplicação prática da interface gráfica

--- src/br/com/refatorandofacilitador/ui/AppWindow.java
+++ src/br/com/refatorandofacilitador/ui/AppWindow.java
@@ -1,8 +1,12 @@
 package br.com.refatorandofacilitador.ui;
+import br.com.refatorandofacilitador.file.CsvModel;
+
 import javax.swing.*;
 
 public class AppWindow {
     public static void interactionInterface() {
+        CsvModel model = new CsvModel();
+
         JFrame frame = new JFrame("Projeto Facilitador - Natus");
         frame.setSize(400, 400);
         frame.setVisible(true);
@@ -13,6 +17,7 @@ public class AppWindow {
 
         JTextField campoDescricao = new JTextField();
         campoDescricao.setEditable(false);
+        campoDescricao.setEnabled(false);
         campoDescricao.setBounds(33, 69, 320, 20);
         frame.add(campoDescricao);
 
@@ -27,5 +32,9 @@ public class AppWindow {
         JTextField campoQuantidade = new JTextField();
         campoQuantidade.setBounds(253, 100, 100,20);
         frame.add(campoQuantidade);
+
+        JButton botaoInserir = new JButton("Inserir");
+        botaoInserir.setBounds(33, 131, 100, 23);
+        frame.add(botaoInserir);
     }
 }
